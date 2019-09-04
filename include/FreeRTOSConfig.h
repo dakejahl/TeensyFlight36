11f7edc84d87c17d79f5c04783d081a14b28acf3
Jacob Dahl <dahl.jakejacob@gmail.com>
2019-09-03 18:49:49 -0600

    Increased FreeRTOS prioties to 5

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -89,7 +89,7 @@ turn the tick off. */
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION	0
 #define configUSE_QUEUE_SETS					1
 #define configCPU_CLOCK_HZ						(F_CPU)
-#define configMAX_PRIORITIES					( 3 )
+#define configMAX_PRIORITIES					( 5 )
 #define configMINIMAL_STACK_SIZE				( ( unsigned short ) 128 )
 #define configMAX_TASK_NAME_LEN					( 20 )
 #define configUSE_16_BIT_TICKS					0
